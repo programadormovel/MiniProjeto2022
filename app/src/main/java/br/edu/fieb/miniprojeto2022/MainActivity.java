ae1cd1194d86d757880cfe7498f1f1942f30e5a5
Adriano <programadormovel@gmail.com>
2022-08-10 16:17:13 -0300

    Menu Lateral

--- app/src/main/java/br/edu/fieb/miniprojeto2022/MainActivity.java
+++ app/src/main/java/br/edu/fieb/miniprojeto2022/MainActivity.java
@@ -2,7 +2,9 @@ package br.edu.fieb.miniprojeto2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Transição de tela
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Ir para a tela do Menu
+                Intent it = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(it);
+            }
+        }, 2000);
     }
 }
